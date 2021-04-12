02ad1605c4d2a8976eea934ca9e41fe1d73dba20
Dan Fisher <dan.fisher85@yahoo.com>
2021-04-12 20:03:14 +0200

    eslint fix

--- src/view/no-film.js
+++ src/view/no-film.js
@@ -4,7 +4,7 @@ const createNoFilmTemplate = () => {
   return `<section class="films-list">
       <h2 class="films-list__title">There are no movies in our database</h2>
     </section>`;
-}
+};
 
 export default class NoFilm {
   constructor() {
