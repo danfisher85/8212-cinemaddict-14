aa136ff98146c66a3d23c652c56e19d826c0b2dc
Dan Fisher <dan.fisher85@yahoo.com>
2021-04-14 15:23:32 +0200

    adds inheritance to simple components

--- src/view/main-nav-stats.js
+++ src/view/main-nav-stats.js
@@ -1,27 +1,11 @@
-import {createElement} from '../utils.js';
+import AbstractView from './abstract.js';
 
 const createNavStatsTemplate = () => {
   return '<a href="#stats" class="main-navigation__additional">Stats</a>';
 };
 
-export default class NavStats {
-  constructor() {
-    this._element = null;
-  }
-
+export default class NavStats extends AbstractView {
   getTemplate() {
     return createNavStatsTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
